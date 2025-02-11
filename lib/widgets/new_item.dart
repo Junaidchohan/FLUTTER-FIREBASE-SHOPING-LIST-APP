64882fa14fd0b8772b8aca0ebb3af7b3d69ea5d6
Muhammad Junaid <junaidchohanedu@gmail.com>
2025-02-11 05:30:31 +0500

    Get Form Access Via a Global Key

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -10,6 +10,12 @@ class NewItemScreen extends StatefulWidget {
 }
 
 class _NewItemScreenState extends State<NewItemScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  void _saveItem() {
+    _formKey.currentState!.validate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,65 +23,80 @@ class _NewItemScreenState extends State<NewItemScreen> {
         title: Text("Add new item"),
       ),
       body: Form(
+          key: _formKey,
           child: Column(
-        children: [
-          TextFormField(
-            maxLength: 50,
-            decoration: InputDecoration(
-              label: Text("Name"),
-            ),
-            validator: (value) {
-              if (value == null ||
-                  value.isEmpty ||
-                  value.trim().length <= 1 ||
-                  value.trim().length > 50) {
-                return "Must be between 1 and 50 charecters";
-              }
-              return null;
-            },
-          ),
-          Row(
             children: [
-              Expanded(
-                child: TextFormField(
-                  decoration: InputDecoration(label: Text("Quantity")),
-                  initialValue: "1",
+              TextFormField(
+                maxLength: 50,
+                decoration: InputDecoration(
+                  label: Text("Name"),
                 ),
+                validator: (value) {
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length <= 1 ||
+                      value.trim().length > 50) {
+                    return "Must be between 1 and 50 charecters.";
+                  }
+                  return null;
+                },
               ),
-              SizedBox(
-                width: 8,
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(label: Text("Quantity")),
+                      keyboardType: TextInputType.number,
+                      initialValue: "1",
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.tryParse(value)! <= 0) {
+                          return "Muet be a valid, positive number.";
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                    child: DropdownButtonFormField(items: [
+                      for (final Category in categories.entries)
+                        DropdownMenuItem(
+                            value: Category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: Category.value.color,
+                                ),
+                                SizedBox(
+                                  width: 6,
+                                ),
+                                Text(Category.value.title)
+                              ],
+                            ))
+                    ], onChanged: (value) {}),
+                  ),
+                ],
               ),
-              Expanded(
-                child: DropdownButtonFormField(items: [
-                  for (final Category in categories.entries)
-                    DropdownMenuItem(
-                        value: Category.value,
-                        child: Row(
-                          children: [
-                            Container(
-                              width: 16,
-                              height: 16,
-                              color: Category.value.color,
-                            ),
-                            SizedBox(
-                              width: 6,
-                            ),
-                            Text(Category.value.title)
-                          ],
-                        ))
-                ], onChanged: (value) {}),
-              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                      onPressed: () {
+                        _formKey.currentState!.reset();
+                      },
+                      child: Text("Reset")),
+                  ElevatedButton(onPressed: _saveItem, child: Text("Submited"))
+                ],
+              )
             ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              TextButton(onPressed: () {}, child: Text("Reset")),
-              ElevatedButton(onPressed: () {}, child: Text("Submited"))
-            ],
-          )
-        ],
-      )),
+          )),
     );
   }
 }
