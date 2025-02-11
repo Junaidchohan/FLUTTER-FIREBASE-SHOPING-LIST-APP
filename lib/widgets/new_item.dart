7e2dc0907154cffeb4ea9fc3e3f24b891d1874fa
Muhammad Junaid <junaidchohanedu@gmail.com>
2025-02-12 02:56:55 +0500

    Working with the requset and waitting for the response

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -19,30 +19,39 @@ class _NewItemScreenState extends State<NewItemScreen> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
-    _formKey.currentState!.validate();
-    _formKey.currentState!.save();
-    final url = Uri.https(
-        'flutter-prep-19e39-default-rtdb.firebaseio.com', 'Shopping-list.json');
-    http.post(
-      url,
-      headers: {'Content-Type': 'application/json'},
-      body: json.encode(
-        {
-          'name:': _enteredName,
-          'quantity': _enteredQuantity,
-          'category': _selectedCategory.title,
-        },
-      ),
-    );
-    // Navigator.of(context).pop(
-    //   GroceryItem(
-    //     id: DateTime.now().toString(),
-    //     name: _enteredName,
-    //     quantity: _enteredQuantity,
-    //     category: _selectedCategory,
-    //   ),
-    // );
+  void _saveItem() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      final url = Uri.https('flutter-prep-19e39-default-rtdb.firebaseio.com',
+          'Shopping-list.json');
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode(
+          {
+            'name:': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory.title,
+          },
+        ),
+      );
+      print(response.body);
+      print(response.statusCode);
+
+      if (context.mounted) {
+        return;
+      }
+
+      Navigator.of(context).pop();
+      // (
+      //   GroceryItem(
+      //     id: DateTime.now().toString(),
+      //     name: _enteredName,
+      //     quantity: _enteredQuantity,
+      //     category: _selectedCategory,
+      //   ),
+      // );
+    }
   }
 
   @override
